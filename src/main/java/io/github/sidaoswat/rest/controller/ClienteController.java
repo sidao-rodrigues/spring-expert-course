3df985e933d90d32e3d91870ce71cbaba9c7828a
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-05-24 11:57:40 -0300

    Implementando metodo de atualizar

--- src/main/java/io/github/sidaoswat/rest/controller/ClienteController.java
+++ src/main/java/io/github/sidaoswat/rest/controller/ClienteController.java
@@ -46,4 +46,15 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity update( @PathVariable("id") Integer id, @RequestBody Cliente cliente){
+        return clientes.findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return  ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build() );
+    }
+
 }
