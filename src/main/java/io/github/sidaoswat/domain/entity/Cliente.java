089630172a82f1f0b546ed014612a407f2a30c77
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-05-23 22:24:22 -0300

    mapeando entidade Cliente com anotações JPa

--- src/main/java/io/github/sidaoswat/domain/entity/Cliente.java
+++ src/main/java/io/github/sidaoswat/domain/entity/Cliente.java
@@ -1,8 +1,17 @@
 package io.github.sidaoswat.domain.entity;
 
+import javax.persistence.*;
+
+@Entity //essa anotação servirá para ser escaneada pelo JPA e registrar essa entidade(classe) como uma tabela no banco de dados
+@Table(name = "cliente")//, schema = "vendas") //essa anotação é para o caso a tabela do bd seja diferente do nome da classe
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id") //essa anotação serve para identificar a coluna da tabela, é opcional mas caso o nome seja diferente tem que se utilizar
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
 
     public Cliente(){
