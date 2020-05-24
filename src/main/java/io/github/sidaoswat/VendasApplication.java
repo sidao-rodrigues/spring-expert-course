a7d20f4be9ddfb626a2159fdd0c6e9960e3be945
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-05-24 11:08:33 -0300

    Criando controller de clientes

--- src/main/java/io/github/sidaoswat/VendasApplication.java
+++ src/main/java/io/github/sidaoswat/VendasApplication.java
@@ -1,43 +1,10 @@
 package io.github.sidaoswat;
 
-import io.github.sidaoswat.domain.entity.Cliente;
-import io.github.sidaoswat.domain.entity.Pedido;
-import io.github.sidaoswat.domain.repository.Clientes;
-import io.github.sidaoswat.domain.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes, @Autowired Pedidos pedidos){
-        return args -> {
-            System.out.println("Save");
-            Cliente cli = new Cliente("Sidney Rodrigues");
-            clientes.save(cli);
-
-            Pedido p = new Pedido();
-            p.setCliente(cli);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-            /*Cliente cliente = clientes.findClienteFetchPedidos(cli.getId());
-            System.out.println(cliente);
-            System.out.println(cliente.getPedidos());*/
-            pedidos.findByCliente(cli).forEach(System.out::println);
-
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- src/main/java/io/github/sidaoswat/domain/repository/Pedidos.java
+++ src/main/java/io/github/sidaoswat/domain/repository/Pedidos.java
@@ -5,7 +5,6 @@ import io.github.sidaoswat.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface Pedidos extends JpaRepository<Pedido, Integer> { //classe e chave da tabela
     List<Pedido> findByCliente(Cliente cliente);
