ac18052da92012f44f157df176b376eb96624bb2
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-05-31 17:07:02 -0300

    configurando metodo de autenticacao de usuario em SecurityConfig

--- src/main/java/io/github/sidaoswat/config/SecurityConfig.java
+++ src/main/java/io/github/sidaoswat/config/SecurityConfig.java
@@ -1,16 +1,42 @@
 package io.github.sidaoswat.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();//gera um hash sempre diferente mesmo para senha iguais
+    }
+        //Essa parte serve para caso eu queria implementar minha propria forma de criptografia
+        /*PasswordEncoder passwordEncoder = new PasswordEncoder() {
+
+            @Override
+            public String encode(CharSequence charSequence) {
+                return null;
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return false;
+            }
+        }*/
+    //}
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("sidao")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER");
     }
 
     @Override
