9d2b39ca9bbaf20219bfcd3130942bebe7bfd710
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2020-06-01 12:44:07 -0300

    implementando metodo de decodificar token

--- src/main/java/io/github/sidaoswat/JwtService.java
+++ src/main/java/io/github/sidaoswat/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.sidaoswat;
 
 import io.github.sidaoswat.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -25,12 +28,44 @@ public class JwtService {
         LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusMinutes(expString);
         Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
         Date data = Date.from(instant);
+
+        /* //para colocar outras claims no token
+        HashMap<String, Object> claims = new HashMap<>();
+        claims.put("emaildousuario", "usuario@mail.com");
+        claims.put("roles", "admin");*/
+
         return Jwts
                 .builder()
                 .setSubject(usuario.getLogin())
                 .setExpiration(data)
+                //.setClaims(claims)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)//o algoritmo pode escolher, juntamente com a chave
                 .compact();
+    }
+
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException {
+        return (String) obterClaims(token).getSubject();
+    }
+
+    public boolean tokenValido(String token){
+        try{
+            Claims claims = obterClaims(token);
+            Date dataExpiration = claims.getExpiration();
+            LocalDateTime data = dataExpiration.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+            return !LocalDateTime.now().isAfter(data);
+
+        }catch (Exception e){
+            return false;
+        }
     }
 
 }
